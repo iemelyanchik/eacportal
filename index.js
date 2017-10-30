d12d394412f4e73286949d30b030facb0c92a178
iemelyanchik <iemelyanchik@gmail.com>
2017-10-31 01:40:55 +0300

    new instance

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.sendFile(__dirname + '/index.html');
 });
 
 app.post('/mail', function(request, response) {
