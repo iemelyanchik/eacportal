f6a5bad3160a852b89f55a74e73400f0fee046c5
iemelyanchik <iemelyanchik@gmail.com>
2017-10-31 15:32:35 +0300

    removing node_m

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
-const nodemailer = require('nodemailer');
+var express = require('express');
+var app = express();
+var nodemailer = require('nodemailer');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
 });
 
 app.post('/mail', function(request, response) {
-    let transporter = nodemailer.createTransport({
+    var transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 587,
         secure: false, // true for 465, false for other ports
@@ -29,8 +29,8 @@ app.post('/mail', function(request, response) {
     });
 
 // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Боженька" <info@gmail.com>', // sender address
+    var mailOptions = {
+        from: 'Ваш клиент', // sender address
         to: 'eacportal@eacportal.com', // list of receivers
         subject: 'Тест отправки', // Subject line
         text: 'Какой-то текст', // plain text body
@@ -38,20 +38,15 @@ app.post('/mail', function(request, response) {
     };
 
 // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
+    transporter.sendMail(mailOptions, function(error, info){
         if (error) {
             return console.log(error);
         }
         console.log('Message sent: %s', info.messageId);
 // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
 });
-});
-
-
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
