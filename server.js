69178cad5cb7d874e57b5864d2ddc1374900749d
iemelyanchik <iemelyanchik@gmail.com>
2017-11-02 13:53:13 +0300

    changes

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var nodemailer = require('nodemailer');
 
-app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/'));
 
 // views is directory for all template files
@@ -48,6 +46,8 @@ app.post('/mail', function(request, response) {
     });
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+console.log('Press Ctrl+C to quit.');
 });
